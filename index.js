24cad17e5cb81e2843e618598260b5cf6f094fc4
leesangjik <sangjik9206@gmail.com>
2020-03-13 16:49:07 +0900

    step_2
    create server

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hi");
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+function handleListening(){
+    console.log(`listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res){
+    res.send("Hello Lee");
+}
+
+function handleProfile(req, res){
+    res.send("U r on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
