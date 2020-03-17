cc04a9f137f878f829d244f4b1fa8cb7f2a2806d
leesangjik <sangjik9206@gmail.com>
2020-03-17 21:16:58 +0900

    step
    router

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import "core-js";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { userRouter } from "./router";
+const app = express();
+
+const handleHome = (req, res) => res.send("Hello Lee");
+
+const handleProfile = (req, res) => res.send("U r on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"))
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.use("/user", userRouter);
+
+export default app;
