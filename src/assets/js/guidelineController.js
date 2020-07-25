46e43cd9301298dd77c3f23f931c2465d5ad4af0
leesangjik <sangjik9206@gmail.com>
2020-07-26 01:01:42 +0900

    semi-complete video detail

--- src/assets/js/guidelineController.js
+++ src/assets/js/guidelineController.js
@@ -1,18 +1,11 @@
-const burgerIcon = document.getElementById("burgerIcon");
-const guideLine = document.getElementById("guideLine");
+const guideHome = document.getElementById("homeIcon");
 
-const handleguideLineIcon = () => {
-    if (burgerIcon.classList.contains("opened") === true) {
-        burgerIcon.classList.replace("opened", "closed");
-        guideLine.classList.replace("opened", "closed");
-    } else {
-        burgerIcon.classList.replace("closed", "opened");
-        guideLine.classList.replace("closed", "opened");
-    }
+const handleHomeIcon = () => {
+    //console.dir(guideHome);
 }
 
 const init = () => {
-    burgerIcon.addEventListener("click", handleguideLineIcon);
-}
+    guideHome.addEventListener("mousemove", handleHomeIcon);
+};
 
-//init()
+init();
